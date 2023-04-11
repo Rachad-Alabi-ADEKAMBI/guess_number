7bf3037f5fb79c3bc4e1c81c44e3d25f6804a062
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2023-04-11 20:13:05 +0100

    update

--- main.py
+++ main.py
@@ -11,4 +11,18 @@ def guess(x):
             print('Sorry, guess again, Too high !')
 
     print(f'Congrats, you guessed {random_number} correctly')
+
+def computer_guess(x):
+    low =1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        guess = random.randint(low, high)
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (c) ?')
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess +1
+        print (f'Yes ! thie computer guessed the number {guess} correctly ')
+
 guess(10)
