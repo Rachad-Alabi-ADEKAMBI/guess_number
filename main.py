3687ad3f566910a5857d7e666e4c3b60c10d38c6
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2023-04-12 16:38:13 +0100

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+
 def guess(x):
     random_number = random.randint(1, x)
     guess = 0
@@ -12,17 +13,24 @@ def guess(x):
 
     print(f'Congrats, you guessed {random_number} correctly')
 
+
 def computer_guess(x):
-    low =1
+    low = 1
     high = x
     feedback = ''
     while feedback != 'c':
-        guess = random.randint(low, high)
-        feedback = input(f'Is {guess} too high (H), too low (L), or correct (c) ?')
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(
+            f'Is {guess} too high (H), too low (L), or correct (c) ?')
         if feedback == 'h':
             high = guess - 1
         elif feedback == 'l':
-            low = guess +1
-        print (f'Yes ! thie computer guessed the number {guess} correctly ')
+            low = guess + 1
 
-guess(10)
+    print(f'Yes ! the computer guessed the number {guess} correctly ')
+
+
+computer_guess(20)
